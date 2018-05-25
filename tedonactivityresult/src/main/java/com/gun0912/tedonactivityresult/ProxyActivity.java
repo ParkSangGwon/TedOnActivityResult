cc2cba49a42bd2dfd0070aafedcc699e23420c77
tedpark <gun0912@naver.com>
2018-05-25 20:36:41 +0900

    - activityRequestStack NPE handling

--- tedonactivityresult/src/main/java/com/gun0912/tedonactivityresult/ProxyActivity.java
+++ tedonactivityresult/src/main/java/com/gun0912/tedonactivityresult/ProxyActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+
 import com.gun0912.tedonactivityresult.listener.OnActivityResultListener;
 import com.gun0912.tedonactivityresult.model.ActivityRequest;
 
@@ -14,43 +15,47 @@ import java.util.Random;
 
 public class ProxyActivity extends Activity {
 
-  private static Deque<ActivityRequest> activityRequestStack;
+    private static Deque<ActivityRequest> activityRequestStack;
 
 
-  public static void startActivityForResult(Context context, Intent intent, OnActivityResultListener listener) {
+    public static void startActivityForResult(Context context, Intent intent, OnActivityResultListener listener) {
 
-    if (activityRequestStack == null) {
-      activityRequestStack = new ArrayDeque<>();
+        if (activityRequestStack == null) {
+            activityRequestStack = new ArrayDeque<>();
+        }
+
+        ActivityRequest activityRequest = new ActivityRequest(intent, listener);
+        activityRequestStack.push(activityRequest);
+
+        Intent tempIntent = new Intent(context, ProxyActivity.class).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        context.startActivity(tempIntent);
     }
 
-    ActivityRequest activityRequest = new ActivityRequest(intent, listener);
-    activityRequestStack.push(activityRequest);
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-    Intent tempIntent = new Intent(context, ProxyActivity.class).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-    context.startActivity(tempIntent);
-  }
+        if (activityRequestStack == null) {
+            finish();
+            return;
+        }
+        ActivityRequest activityRequest = activityRequestStack.peek();
 
-  @Override
-  protected void onCreate(@Nullable Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-
-    ActivityRequest activityRequest = activityRequestStack.peek();
-
-    Intent intent = activityRequest.getIntent();
-    super.startActivityForResult(intent, new Random().nextInt(65536));
-  }
-
-  @Override
-  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    ActivityRequest activityRequest = activityRequestStack.pop();
-    OnActivityResultListener listener = activityRequest.getListener();
-
-    listener.onActivityResult(resultCode, data);
-
-    if (activityRequestStack.size() == 0) {
-      activityRequestStack = null;
+        Intent intent = activityRequest.getIntent();
+        super.startActivityForResult(intent, new Random().nextInt(65536));
     }
 
-    finish();
-  }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        ActivityRequest activityRequest = activityRequestStack.pop();
+        OnActivityResultListener listener = activityRequest.getListener();
+
+        listener.onActivityResult(resultCode, data);
+
+        if (activityRequestStack.size() == 0) {
+            activityRequestStack = null;
+        }
+
+        finish();
+    }
 }
